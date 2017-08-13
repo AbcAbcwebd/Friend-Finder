b51758428867ff8ac96afd7a2fc7da129690d23f
Owen Essen <abc_abcwebd@yahoo.com>
2017-08-12 20:56:29 -0400

    External API routing works.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var path = require('path');
 var app = express();
+var quizObj = require('./../data/friends.js')
 
 app.get('/api/quizes', function(req,res){
-	res.json(path.join(__dirname,'../data/friends.js'));
+	res.json(quizObj);
 });
 
 module.exports = app;
