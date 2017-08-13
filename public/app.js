dbb2d87952492165e336b0d2690c1a1c020da8dd
Owen Essen <abc_abcwebd@yahoo.com>
2017-08-12 21:11:36 -0400

    Quiz generation now works.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
 function generateQuiz(quiz){
-	
-}
+	$.ajax({
+	      url: '/api/quizes',
+	      method: "GET"
+	    }).done(function(info) {
+			var activeQuiz = info[quiz];
+			for (var i = 0; i < activeQuiz.questions.length; i++){
+				var surveyElement = '<p class="question">' + activeQuiz.questions[i] + '</p><input type="range" max="5" min="1" class="input-slider"><br><p class="diss">Strongly Disagree</p><p class="agr">Strongly Agree</p><br><br>';
+				$('#survey-form').append(surveyElement);
+			}
+
+		  
+	});
+};
